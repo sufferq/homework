7dfb7885d72b6fe04c97752e5f5099eb330ec4b5
sufferq <semenbulavin@yandex.ru>
2024-09-05 16:20:46 +0500

    Delete module_1_4.py

--- module_1_4.py
+++
@@ -1,9 +0,0 @@
-my_string = (input('Введи текст: '))
-print ('В этом тексте', (len(my_string)), 'символов.')
-print (my_string .upper())
-print (my_string .lower())
-print (my_string .replace (' ', '' ))
-print (my_string [0])
-print (my_string [-1])
-
-
