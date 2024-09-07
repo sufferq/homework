841688e7406e01ff22a3a7291e722016e9640d2e
sufferq <semenbulavin@yandex.ru>
2024-09-07 16:10:40 +0500

    Add files via upload

---
+++ module_1.6.py
@@ -0,0 +1,18 @@
+my_dict = {'Dima': 2005, 'Alisa': 2007}
+print (my_dict)
+print (my_dict ['Dima'])
+print (my_dict.get ('Semyon'))
+print (my_dict)
+my_dict.update ({'Artem': 2007,
+                 'Sergei': 2006})
+print (my_dict)
+print (my_dict.pop ('Alisa'))
+print (my_dict)
+
+#Множества
+my_set = {1, 2, 3, 4, 4, 5, 5, 'String', 42.5, 'String'}
+print (my_set)
+print (my_set.add ('Elephant'))
+print (my_set.add (8))
+print (my_set.discard(1))
+print (my_set)
