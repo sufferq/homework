ae82056b0ff3b2ddc9b5c35416f85a6b247d4236
sufferq <semenbulavin@yandex.ru>
2024-09-27 00:50:10 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,11 @@
+def divide(first, second):
+    if second != 0:
+        result = first / second
+        return result
+    else:
+        return "Ошибка"
+
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        result1 = first / second
+        return result1
+    else:
+        return inf
+
