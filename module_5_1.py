007fccb809f616457497c5431099876c4d0eaea5
sufferq <semenbulavin@yandex.ru>
2024-10-04 20:55:12 +0500

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -5,14 +5,30 @@ class House:
 
     def go_to(self, new_floor):
         if 0 < new_floor <= self.number_of_floors:
-            for floor in range(1, new_floor ):
+            for floor in range(1, new_floor):
                 print(floor)
             else:
                 print('Такого этажа не существует')
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (f'Название {self.name}, кол-во этажей {self.number_of_floors}')
 
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
