ba599e4ab68c30328ec06b7efdbdc6a56f91d89d
sufferq <semenbulavin@yandex.ru>
2024-12-06 19:19:52 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+example1 = (2 * 2 + 2)
+example2 = (2 * (2 + 2))
+print(example1 == example2)
+
+#4th program
+print(int(float('123.456')*10)%10)
+
